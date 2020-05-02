66b78bcfa4e08349d3a5b8825059de36ea464af2
Travis Fedora PC <travis@zenperfectdesign.com>
2020-05-02 11:14:13 +0200

    Added woff2 extension ro resource patterns in config file.

--- app/config.php
+++ app/config.php
@@ -4,6 +4,6 @@ return [
 
     'bot.pattern'           => '/(bot|spider|crawl|scanner|scan|qwant|grab)/i',
     'unusual.agent.pattern' => '/(python|perl|urllib|curl|^\-$|zgrab|postman)/i',
-    'resource.pattern'      => '/(.+)(\.\w{1,3}|woff|jpeg)$/i'
+    'resource.pattern'      => '/(.+)(\.\w{1,3}|woff|jpeg|woff2)$/i'
 
 ];
